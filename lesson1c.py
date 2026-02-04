a7d31f6fc2ba04ed7d533f515a7442dcce124de4
victormuchunu-cyber <victor.muchunu@gmail.com>
2026-02-04 13:59:25 +0300

    lesson1c

--- lesson1c.py
+++ lesson1c.py
@@ -4,3 +4,40 @@ number1 =10
 number2 =20
 addition = number1 + number2
 print("The sum of the two numbers is: ", addition)
+
+
+#subtraction
+number3 =50
+number4 =15
+difference = number3 - number4
+print("The difference of the numbers is: ", difference)
+
+#multplication
+number5 =10
+number6 =6
+product = number5 * number6
+print("The product of the number is: ", product)
+
+#division
+number7 =10
+number8 =5
+quotient = number7 / number8
+print("The quotient of the number is: ", quotient)
+
+
+#modulus- This means the remainder of a number when get subdivide by another one
+number9 =14
+number10 =5
+modulus = number9 % number10
+print("The modulus of the number is: ", modulus)
+
+#Floor Division- This is the number of times a number subdivides another ignoring the remainder
+floor =number9 // number10
+print("The floor division of the two numbers is: ", floor)
+
+
+#Exponent (**)
+#The power of a number
+number11 =10
+exponent = number11 **2 
+print("The exponent of the number is: ", exponent)
