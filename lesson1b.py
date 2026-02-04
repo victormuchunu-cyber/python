ceeaf082e8c143fba858313669d8a060291bf49d
victormuchunu-cyber <victor.muchunu@gmail.com>
2026-02-04 13:33:41 +0300

    updated code

---
+++ lesson1b.py
@@ -0,0 +1,19 @@
+# Python Data types
+# String- It is a series of characters that are inside of quotes.
+name ="Joseph Mwangi"
+print(name)
+print(type(name))
+
+# Numeric
+# Int - This indicate a whole number in python. For an integer, we don't use quotes.
+age = 39
+print("the age of a person is:", age)
+print(type(age))
+
+# float- this is a number with a decimal.
+coordinates = 15.4524
+print("The coordinates are: ", coordinates)
+print(type(coordinates))
+
+
+#Research and come up with an example of a comlex data type
---
+++ lesson1c.py
@@ -0,0 +1,6 @@
+# Arithmetic
+#Addition
+number1 =10
+number2 =20
+addition = number1 + number2
+print("The sum of the two numbers is: ", addition)
