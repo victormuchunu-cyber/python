48b11828c3145ad77a5d5d672f1e769931ea8d6f
victormuchunu-cyber <victor.muchunu@gmail.com>
2026-02-04 14:15:30 +0300

    lesson1a

---
+++ assignment1.py
@@ -0,0 +1,5 @@
+
+# Assignment
+# 1. Write a python program that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 meters. BMI = (weight) / (height squared)
+# 2. Find the Area and perimeter of a rectangle whose length is 48cm and width is 25cm
+# Research on python list, tuple and Dictionary Data types.
---
+++ lesson1d.py
@@ -0,0 +1,8 @@
+#Create a python program that is able to calculate the simple interest given the principle as 50,000, rate as 5% and time as 8 years. SI= (P * R * T)/100
+
+principle= 50000
+rate=5
+time=8
+SI= (principle * rate * time)/100
+print("The SI of the numbers is: ", SI)
+
