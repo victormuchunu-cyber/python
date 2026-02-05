4191eed9cf55055dd86f2175e0ca6f8bed93a313
victormuchunu-cyber <victor.muchunu@gmail.com>
2026-02-05 11:43:54 +0300

    updated code

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,23 @@
 
 # Assignment
 # 1. Write a python program that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 meters. BMI = (weight) / (height squared)
+
+weight = 78    
+height = 1.75
+
+bmi = (weight) / (height ** 2)
+print("The BMI is: ", bmi)
 # 2. Find the Area and perimeter of a rectangle whose length is 48cm and width is 25cm
+
+Length = 48
+Width = 25
+
+area = Length * Width
+perimeter = 2 * (Length + Width)
+
+print("area of the rectangle: ", area)
+print("perimeter of the rectangle: ", perimeter)
 # Research on python list, tuple and Dictionary Data types.
+cars = ["Toyota", "Honda", "BMW", "Mercedes", "Audi", "Nissan", "Ford"]
+
+print(cars)
