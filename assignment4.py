63654d0a3637a94131fb91dcdeff7b76e4ee7388
victormuchunu-cyber <victor.muchunu@gmail.com>
2026-02-10 09:28:36 +0300

    updated code

---
+++ assignment4.py
@@ -0,0 +1,21 @@
+# Lesson4Task1.py
+
+for year in range(2000, 2025):
+    print(year)
+
+
+# Lesson4Task3.py
+
+colors = ["Blue", "Green", "Red", "Pink", "Black"]
+
+for color in colors:
+    print(color)
+
+
+# Lesson4Task2.py
+
+num = 20
+
+while num >= 1:
+    print(num)
+    num -= 1
